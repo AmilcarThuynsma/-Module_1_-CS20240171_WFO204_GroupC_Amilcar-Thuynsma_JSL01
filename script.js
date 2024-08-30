a04890a13b2b2f7134fc2312fc479c0d121e6f62
Amilcar Thuynsma <amilcarthuy@gmail.com>
2024-08-30 03:34:37 +0200

    add validation logic to code

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
+    
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+    const RegExp = /^pet_\d{4}[a-zA-Z]+$/;
+
+
+    if (RegExp.test(input)) {
+        result = 'Valid Syntax.';
+    } else {
+        result = 'Invalid Syntax.'
+    }
 
             document.getElementById('result').innerText = result;
 }
